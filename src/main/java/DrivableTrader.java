75e9cb7c8c19cd8502c10f4d088f21317ccd21b2
Quang Nguyen <133824929+Zitherlight@users.noreply.github.com>
2023-06-11 19:29:45 -0300

    DrivableTrader

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,33 @@
  */
 
 import java.util.List;
+
+public class DrivableTrader<T> extends Trader<Drivable>{
+    /**
+     * Construct a Trader and give them the
+     * inventory, wishlist, and money.
+     *
+     * @param inventory
+     * @param wishlist
+     * @param money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist,
+                  int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable item){
+        if (item instanceof Tradable){
+            return item.getMaxSpeed() + ((Tradable) item).getPrice();
+        }
+        else{
+            return Tradable.MISSING_PRICE;
+        }
+    }
+
+}
